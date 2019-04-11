af26daf107eb5f386c94bc4a670e4e628fb00509
Nitrix - Valentin ROGER <valent.23@hotmail.fr>
2019-04-11 20:57:04 +0200

    init 2nd page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { Page1Component } from './page1/page1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    Page1Component
   ],
   imports: [
     BrowserModule,
